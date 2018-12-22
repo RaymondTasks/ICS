8a88778da8741d04a5d177d4ee5214dcae1c2b0b
Monsoon <yjh1021317464@gmail.com>
2018-12-22 15:51:50 +0800

    partly complete LC3 assembler

--- src/Assembler/Instruction.kt
+++ src/Assembler/Instruction.kt
@@ -1,3 +1,302 @@
 package Assembler
 
+///**
+// * 指令、伪指令和编译器指令表
+// */
+//internal enum class Type {
+//    inst, pseudo, asminst
+////    ADD, AND, NOT,
+////    BR, RET, JMP, JSR, JSRR, RTI,
+////    LD, LDR, LDI, LEA, ST, STR, STI,
+////    TRAP,
+////    _START, _END, _FILL, _BLKW, _STRINGZ,
+////    GETC, OUT, PUTS, IN, PUTSP, HALT
+//}
 
+/**
+ * 字段类型枚举
+ */
+internal enum class WordType {
+    Instruction,
+    BR,     //BR指令需要特殊对待
+    PseudoInstruction,
+    AsmInstruction,
+    Reg,
+    Number,
+    Label,
+    String,
+}
+
+class Instruction(private val asm: Array<String>) {
+
+    var label: String? = null
+    var addr = -1
+    var length = -1
+    lateinit var symbolTable: HashMap<String, Int>
+
+    private lateinit var Inst: String
+
+    private var is_imm = false
+    private var SR1 = -1
+    private var SR2 = -1
+    private var SR = -1
+    private var DR = -1
+    private var BaseR = -1
+    private var imm = -1
+    private var trapvector = -1
+    private var string: String? = null
+
+    init {
+        //分析各个字段的类型
+        val wordType = Array<WordType>(asm.size) { i ->
+            //指令、伪指令、汇编指令和寄存器不区分大小写
+            when (asm[i].toUpperCase()) {
+                in InstTable -> WordType.Instruction
+                in BRTable -> WordType.BR               //BR需要特殊对待
+                in pseudoTable -> WordType.PseudoInstruction
+                in asmInstTable -> WordType.AsmInstruction
+                in regTable -> WordType.Reg
+                else -> {
+                    when (asm[i].first()) {
+                        '#' -> WordType.Number
+                        'x', 'X' -> {
+                            //todo 判定数字
+                            WordType.Number
+                            //todo
+                        }
+                        '\'', '"' -> WordType.String
+                        else -> WordType.Label
+                    }
+                }
+            }
+        }
+        //todo 指令解析
+    }
+
+    /**
+     * 转化为机器码
+     */
+    fun toMachineCode(): ShortArray {
+        //Inst已经转化成了大写
+        when (Inst) {
+            in InstTable -> {
+                var code = (InstTable[Inst] as Int) shl 12
+                when (Inst) {
+                    "ADD", "AND" -> {
+                        code += DR shl 9
+                        code += SR1 shl 6
+                        if (is_imm) {
+                            code += 1 shl 5
+                            if (imm in -16..15) {
+                                code += imm and 0b11_111
+                            } else {
+                                //todo 超出立即数表达范围
+                                throw Exception()
+                            }
+                        } else {
+                            code += SR2
+                        }
+                    }
+                    "NOT" -> {
+                        code += DR shl 9
+                        code += SR1 shl 6
+                    }
+                    "JMP", "RET", "JSRR" -> {
+                        code += BaseR shl 6
+                    }
+                    "JSR" -> {
+                        code += 1 shl 11
+                        if (imm in -1024..1023) {
+                            code += imm and 0b11_111_111_111
+                        } else {
+                            //todo 超出立即数表达范围
+                            throw Exception()
+                        }
+                    }
+                    "RTI" -> {
+                        //do nothing
+                    }
+                    "LD", "LDI", "LEA" -> {
+                        code += DR shl 9
+                        if (imm in -256..255) {
+                            code += imm and 0b111_111_111
+                        } else {
+                            //todo 超出立即数表达范围
+                            throw Exception()
+                        }
+                    }
+                    "LDR" -> {
+                        code += DR shl 9
+                        code += BaseR shl 6
+                        if (imm in -32..31) {
+                            code += imm and 0b111_111
+                        } else {
+                            //todo 超出立即数表达范围
+                            throw Exception()
+                        }
+                    }
+                    "ST", "STI" -> {
+                        code += SR shl 9
+                        if (imm in -256..255) {
+                            code += imm and 0b111_111_111
+                        } else {
+                            //todo 超出立即数表达范围
+                            throw Exception()
+                        }
+                    }
+                    "STR" -> {
+                        code += SR shl 9
+                        code += BaseR shl 6
+                        if (imm in -32..31) {
+                            code += imm and 0b111_111
+                        } else {
+                            //todo 超出立即数表达范围
+                            throw Exception()
+                        }
+                    }
+                    "TRAP" -> {
+                        if (trapvector in 0..255) {
+                            code += trapvector and 0b11_111_111
+                        } else {
+                            //todo 超出立即数表达范围
+                            throw Exception()
+                        }
+                    }
+                }
+                return shortArrayOf(code.toShort())
+            }
+            in BRTable -> {
+                var code = (BRTable[Inst] as Int) shl 9
+                if (imm in -256..255) {
+                    code += imm and 0b111_111_111
+                } else {
+                    //todo 超出立即数表达范围
+                    throw Exception()
+                }
+                return shortArrayOf(code.toShort())
+            }
+            in pseudoTable -> {
+                var code = 0b1111 shl 12
+                code += (pseudoTable[Inst] as Int) and 0b11_111_111
+                return shortArrayOf(code.toShort())
+            }
+            in asmInstTable -> {
+                when (Inst) {
+                    ".START", ".END" -> {
+                        return shortArrayOf()
+                    }
+                    ".FILL" -> {
+                        if (imm in -32768..32767) {
+                            return shortArrayOf(imm.toShort())
+                        } else {
+                            //todo 超出立即数表达范围
+                            throw Exception()
+                        }
+                    }
+                    ".BLKW" -> {
+                        if (imm >= 0) {
+                            return ShortArray(imm) { 0 }
+                        } else {
+                            throw Exception()
+                        }
+                    }
+                    ".STRINGZ" -> {
+                        return ShortArray(imm) { i ->
+                            string!![i].toShort()
+                        }
+                    }
+                }
+
+            }
+
+        }
+        //todo 非法指令
+        throw Exception()
+    }
+
+}
+
+internal fun parseNumber(str: String): Int {
+
+}
+
+internal fun parseString(str: String): String {
+
+}
+
+//指令与操作码对照表
+internal val InstTable = hashMapOf(
+        Pair("ADD", 0b0001),
+        Pair("AND", 0b0101),
+        Pair("NOT", 0b1001),
+//        Pair("BR", 0b0000), //BR特殊对待
+        Pair("RET", 0b1100),
+        Pair("JMP", 0b1100),
+        Pair("JSR", 0b0100),
+        Pair("JSRR", 0b0100),
+        Pair("RTI", 0b1000),
+        Pair("LD", 0b0010),
+        Pair("LDR", 0b0110),
+        Pair("LDI", 0b1010),
+        Pair("LEA", 0b1110),
+        Pair("ST", 0b0011),
+        Pair("STR", 0b0111),
+        Pair("STI", 0b1011),
+        Pair("TRAP", 0b1111)
+)
+
+//BR指令与nzp对照表
+internal val BRTable = hashMapOf(
+        Pair("BR", 0b000),
+        Pair("BRN", 0b100),
+        Pair("BRZ", 0b010),
+        Pair("BRP", 0b001),
+        Pair("BRNZ", 0b110),
+        Pair("BRZN", 0b110),
+        Pair("BRNP", 0b101),
+        Pair("BRPN", 0b101),
+        Pair("BRZP", 0b011),
+        Pair("BRPZ", 0b011),
+        Pair("BRNZP", 0b111),
+        Pair("BRNPZ", 0b111),
+        Pair("BRZNP", 0b111),
+        Pair("BRZPN", 0b111),
+        Pair("BRPNZ", 0b111),
+        Pair("BRPZN", 0b111)
+)
+
+//伪指令与TRAP vector对照表
+internal val pseudoTable = hashMapOf(
+        Pair("GETC", 0x20),
+        Pair("OUT", 0x21),
+        Pair("PUTS", 0x22),
+        Pair("IN", 0x23),
+        Pair("PUTSP", 0x24),
+        Pair("HALT", 0x25)
+)
+
+//寄存器对照表
+internal val regTable = hashMapOf(
+        Pair("R0", 0),
+        Pair("R1", 1),
+        Pair("R2", 2),
+        Pair("R3", 3),
+        Pair("R4", 4),
+        Pair("R5", 5),
+        Pair("R6", 6),
+        Pair("R7", 7)
+)
+
+//汇编器指令枚举
+internal enum class asmInstType {
+    _START, _END, _FILL, _BLKW, _STRINGZ
+}
+
+//汇编器指令表
+internal val asmInstTable = hashMapOf(
+        Pair(".STRAT", asmInstType._START),
+        Pair(".END", asmInstType._END),
+        Pair(".FILL", asmInstType._FILL),
+        Pair(".BLKW", asmInstType._BLKW),
+        Pair(".STRINGZ", asmInstType._STRINGZ)
+)
--- src/Assembler/Main.kt
+++ src/Assembler/Main.kt
@@ -1,175 +1,306 @@
 package Assembler
 
+/**
+ * main函数
+ * 汇编选项
+ * [-o] [输出路径]
+ * [-b] 生成文本形式的二进制码
+ * [-h] 生成文本形式的十六进制码
+ * [--help] 显示帮助
+ */
 fun maim(args: Array<String>) {
 
 }
 
 /**
- * 对汇编码进行初步的格式化
+ * 格式化汇编码并转化为指令对象
+ * 指令和寄存器忽略大小写
+ * label和字符串大小写敏感
  * @param asm 未格式化的汇编码
+ * @return 指令对象列表
  */
-fun format(asm: StringBuilder): Array<String> {
-    //去注释
-    //todo 不能去除""中的分号
-    asm.replace(Regex(";.*\n"), " ")
-    asm.replace(Regex("(^\\s+)|(\\s+$)"), "")
-    //依照分隔符分割
-    //todo ""引号中的要特殊处理
-    return asm.split(Regex("(\\s*,\\s*)|(\\s+)"), Int.MAX_VALUE).toTypedArray()
+fun format(asm: String): ArrayList<Instruction> {
+    val insts = ArrayList<Instruction>()
+
+    val originalLines = ArrayList<StringBuilder>()
+    //按行分割
+    asm.split(Regex("\\n+"))
+            .forEach {
+                originalLines.add(StringBuilder(it))
+            }
+
+    //生成指令对象
+    originalLines.forEach {
+
+        if (Regex("\\.STRINGZ", RegexOption.IGNORE_CASE) in it) {
+            //存在字符串,需要特殊对待
+            var inString = false       //在字符串中
+            var a = -1      //字符串左边界（包括）
+            var b = -1      //字符串右边界（不包括）
+            //寻找字符串的范围
+            for (i in 0 until it.length) {
+                if (!inString) {
+                    when (it[i]) {
+                        ';' -> it.delete(i, it.length)      //去注释
+                        '\'', '"' -> {          //字符串内容开始
+                            if (a == -1) {
+                                inString = true
+                                a = i
+                            } else {
+                                //todo 出现了两个字符串
+                                throw Exception()
+                            }
+                        }
+                    }
+                } else {
+                    if (it[i] == it[a] && it[i - 1] != '\\') {
+                        inString = false
+                        b = i + 1               //字符串内容结束
+                    }
+                }
+            }
+            if (a == -1 || b == -1) {
+                //todo 字符串未闭合
+                throw Exception()
+            }
+            val tmp = ArrayList<String>()
+            tmp.addAll(it.substring(0, a)
+                    .replace(Regex("(\\s*;.*$)|(^\\s+)|(\\s+$)"), "")
+                    .split(Regex("(\\s*,\\s*)|(\\s+)")))
+            tmp.add(it.substring(a, b))
+            tmp.addAll(it.substring(b)
+                    .replace(Regex("(\\s*;.*$)|(^\\s+)|(\\s+$)"), "")
+                    .split(Regex("(\\s*,\\s*)|(\\s+)")))
+            //todo ???为什么下面这句不能加
+            insts.add(Instruction(tmp.toTypedArray()))
+        } else {
+            //非.STRINGZ
+            //去注释、首位空格
+            it.replace(Regex("(\\s*;.*$)|(^\\s+)|(\\s+$)"), "")
+            //分割字段
+            if (it.toString() != "") {  //排除空行
+                //添加一条有效指令
+                insts.add(Instruction(
+                        it.split(Regex("(\\s*,\\s*)|(\\s+)"))
+                                .toTypedArray()))
+            }
+        }
+
+    }
+    return insts
 }
 
 /**
- * 根据格式化后的汇编码转化为指令对象
- * 汇编码是大小写敏感的
- * @param asm 格式化后的汇编码
+ * 创建符号链接表
+ * @param insts 指令对象列表,去除了.START和.END
+ * @param start 第一条指令所在的位置
+ * @return 符号链接表
  */
-@kotlin.ExperimentalUnsignedTypes
-fun format(asm: Array<String>): Array<Instruction> {
-    var inline: Boolean = false     //标志是否在一条指令中
-    var haveLabel: Boolean = false
-    var type: Instruction
-
-    asm.forEachIndexed { i, str ->
-        if (inline) {
-
-        } else {
-            //下一条指令
-            inline = true
-            when (str.toUpperCase()) {
-                in instTable -> type = Instruction.valueOf(str.toUpperCase())
-                in BRTable ->
-                in pseudoTable ->
-                in asmInstTable ->
-                else ->
-            }
-            if (instTable.contains(str)) {
-                haveLabel = false
-                //BR需要单独处理
-                type = Instruction.valueOf(str.toUpperCase())
-//                type = when (str) {
-//                    "ADD" -> Instruction.ADD
-//                    "AND" -> Instruction.AND
-//                    "NOT" -> Instruction.NOT
-//                    "BR", "BRn", "BRz", "BRp", "BRnz", "BRzn", "BRnp", "BRpn", "BRzp", "BRpz",
-//                    "BRnzp", "BRnpz", "BRznp", "BRzpn", "BRpnz", "BRpzn" -> Instruction.BR
-//                    "JMP" -> Instruction.JMP
-//                    "JSR"->Instruction.JSR
-//                    "JSRR"->
-//                    else -> throw Exception()  //todo 抛出错误
-//                }
-            } else if () else {
-                haveLabel = true
+fun creatSymbolTable(insts: ArrayList<Instruction>, start: Int): HashMap<String, Int> {
+    var addr = start
+    val table = HashMap<String, Int>()
+    insts.forEach {
+        it.addr = addr
+        if (it.label != null) {
+            if (it.label in table) {
+                //todo label重复错误
+                throw Exception()
+            } else {
+                //添加一个label
+                table.put(it.label!!, addr)
             }
         }
+        //更新label
+        addr += it.length
     }
+    insts.forEach {
+        it.symbolTable = table
+    }
+    return table
+}
+
+/**
+ * 生成机器码
+ * @param insts 指令对象列表,去除了.START和.END,更新了label
+ * @return 机器码序列
+ */
+fun generateMachineCode(insts: ArrayList<Instruction>): ArrayList<Short> {
+    val array = ArrayList<Short>()
+    insts.forEach {
+        it.toMachineCode().forEach { code ->
+            array.add(code)
+        }
+    }
+    return array
 }
 
 
-@kotlin.ExperimentalUnsignedTypes
-fun creatSymbolTable(insts: Array<Instruction>): HashMap<String, UShort> {
-    val map = HashMap<String, UShort>()
-
-}
-
-//指令列表
-val instTable = hashSetOf("ADD", "AND", "NOT",
-        "JMP", "JSR", "JSRR", "RET", "RTI",
-        "LD", "LDI", "LDR", "LEA",
-        "ST", "STI", "STR",
-        "TRAP")
-val BRTable = hashSetOf("BR",
-        "BRN", "BRZ", "BRP",
-        "BRNZ", "BRZN", "BRNP", "BRPN", "BRZP", "BRPZ",
-        "BRNZP", "BRNPZ", "BRZNP", "BRZPN", "BRPNZ", "BRPZN")
-val regTable = hashSetOf("R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7")
-val pseudoTable = hashSetOf("GETC", "OUT", "PUTS", "IN", "PUTSP", "HALT")
-val asmInstTable = hashSetOf(".FILL", ".STRINGZ", ".BLKW", ".START", ".END")
-
-@kotlin.ExperimentalUnsignedTypes
-enum class Instruction {
-    ADD {
-        val opCode = 0b0001u
-        override fun toMachineCode(): UShort {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-    },
-    AND {
-        val opCode = 0b0101u
-        override fun toMachineCode(): UShort {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-    },
-    NOT {
-        override fun toMachineCode(): UShort {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-    },
-    //需要特殊对待
-    BR {
-        override fun toMachineCode(): UShort {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-    },
-    JMP {
-
-    },
-    JSR {
-
-    },
-    JSRR {
-
-    },
-    RET {
-
-    },
-    RTI {
-
-    },
-    LD {
-
-    },
-    LDR {
-
-    },
-    LDI {
-
-    },
-    LEA {
-
-    },
-    ST {
-
-    },
-    STR {
-
-    },
-    STI {
-
-    },
-    TRAP {
-
-    },
-    _FILL {
-
-    },
-    _STRINGZ {
-
-    },
-    _BLKW {
-
-    },
-    _START {
-
-    },
-    _END {
-
-    }
-
-    abstract fun toMachineCode: UShort {
-
-    }
-
-}
+///**
+// * 对汇编码进行初步的格式化
+// * @param asm 未格式化的汇编码
+// */
+//fun format(asm: StringBuilder): Array<String> {
+//    //去注释
+//    //todo 不能去除""中的分号
+//    asm.replace(Regex(";.*\n"), " ")
+//    asm.replace(Regex("(^\\s+)|(\\s+$)"), "")
+//    //依照分隔符分割
+//    //todo ""引号中的要特殊处理
+//    return asm.split(Regex("(\\s*,\\s*)|(\\s+)"), Int.MAX_VALUE).toTypedArray()
+//}
+//
+///**
+// * 根据格式化后的汇编码转化为指令对象
+// * 汇编码是大小写敏感的
+// * @param asm 格式化后的汇编码
+// */
+//@kotlin.ExperimentalUnsignedTypes
+//fun format(asm: Array<String>): Array<Instruction> {
+//    var inline: Boolean = false     //标志是否在一条指令中
+//    var haveLabel: Boolean = false
+//    var type: Instruction
+//
+//    asm.forEachIndexed { i, str ->
+//        if (inline) {
+//
+//        } else {
+//            //下一条指令
+//            inline = true
+//            when (str.toUpperCase()) {
+//                in instTable -> type = Instruction.valueOf(str.toUpperCase())
+//                in BRTable ->
+//                in pseudoTable ->
+//                in asmInstTable ->
+//                else ->
+//            }
+//            if (instTable.contains(str)) {
+//                haveLabel = false
+//                //BR需要单独处理
+//                type = Instruction.valueOf(str.toUpperCase())
+////                type = when (str) {
+////                    "ADD" -> Instruction.ADD
+////                    "AND" -> Instruction.AND
+////                    "NOT" -> Instruction.NOT
+////                    "BR", "BRn", "BRz", "BRp", "BRnz", "BRzn", "BRnp", "BRpn", "BRzp", "BRpz",
+////                    "BRnzp", "BRnpz", "BRznp", "BRzpn", "BRpnz", "BRpzn" -> Instruction.BR
+////                    "JMP" -> Instruction.JMP
+////                    "JSR"->Instruction.JSR
+////                    "JSRR"->
+////                    else -> throw Exception()  //todo 抛出错误
+////                }
+//            } else if () else {
+//                haveLabel = true
+//            }
+//        }
+//    }
+//}
+//
+//
+//@kotlin.ExperimentalUnsignedTypes
+//fun creatSymbolTable(insts: Array<Instruction>): HashMap<String, UShort> {
+//    val map = HashMap<String, UShort>()
+//
+//}
+//
+////指令列表
+//val instTable = hashSetOf("ADD", "AND", "NOT",
+//        "JMP", "JSR", "JSRR", "RET", "RTI",
+//        "LD", "LDI", "LDR", "LEA",
+//        "ST", "STI", "STR",
+//        "TRAP")
+//val BRTable = hashSetOf("BR",
+//        "BRN", "BRZ", "BRP",
+//        "BRNZ", "BRZN", "BRNP", "BRPN", "BRZP", "BRPZ",
+//        "BRNZP", "BRNPZ", "BRZNP", "BRZPN", "BRPNZ", "BRPZN")
+//val regTable = hashSetOf("R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7")
+//val pseudoTable = hashSetOf("GETC", "OUT", "PUTS", "IN", "PUTSP", "HALT")
+//val asmInstTable = hashSetOf(".FILL", ".STRINGZ", ".BLKW", ".START", ".END")
+//
+//@kotlin.ExperimentalUnsignedTypes
+//enum class Instruction {
+//    ADD {
+//        val opCode = 0b0001u
+//        override fun toMachineCode(): UShort {
+//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        }
+//    },
+//    AND {
+//        val opCode = 0b0101u
+//        override fun toMachineCode(): UShort {
+//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        }
+//    },
+//    NOT {
+//        override fun toMachineCode(): UShort {
+//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        }
+//    },
+//    //需要特殊对待
+//    BR {
+//        override fun toMachineCode(): UShort {
+//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        }
+//    },
+//    JMP {
+//
+//    },
+//    JSR {
+//
+//    },
+//    JSRR {
+//
+//    },
+//    RET {
+//
+//    },
+//    RTI {
+//
+//    },
+//    LD {
+//
+//    },
+//    LDR {
+//
+//    },
+//    LDI {
+//
+//    },
+//    LEA {
+//
+//    },
+//    ST {
+//
+//    },
+//    STR {
+//
+//    },
+//    STI {
+//
+//    },
+//    TRAP {
+//
+//    },
+//    _FILL {
+//
+//    },
+//    _STRINGZ {
+//
+//    },
+//    _BLKW {
+//
+//    },
+//    _START {
+//
+//    },
+//    _END {
+//
+//    }
+//
+//    abstract fun toMachineCode: UShort {
+//
+//    }
+//
+//}
 
 
 
